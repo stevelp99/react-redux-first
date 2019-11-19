4529dbd25df48686990a96b8f0516605092e97b8
Steven Dismuke <stevelp99@yahoo.com>
2019-11-19 11:48:51 -0600

    added button and connected to count display

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import {Provider} from "react-redux";
 import store from "./redux/store";
-
-
+import CounterDisplay from "./components/counterDisplay";
+import IncrementButton from "./components/incrementButton"
+//npx create-react-app (nameTheApp)
+//npm install redux react-redux
 function App() {
   return (
     <Provider store={store}>
     <div>
       <h1>redux with react</h1>
       <p>simple counter</p>
+      <CounterDisplay/>
+      <IncrementButton/>
     </div>
     </Provider>
   );
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,6 +1,6 @@
 import {INCREMENT,DECREMENT} from "./actionTypes";
 
-export default function reducer(state=10,action){
+export default function reducer(state=0,action){
     switch(action.type){
         case INCREMENT:
             return state+1;
